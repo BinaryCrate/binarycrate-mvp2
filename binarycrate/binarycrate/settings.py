c2d022e67b7c05bb199c65dde52a70b5d2b86b3a
Mark Lockett <mlockett@bigpond.com>
2017-10-02 08:44:41 +0800

    Move the pypyjs release sub module and get the python front end program working

--- binarycrate/binarycrate/settings.py
+++ binarycrate/binarycrate/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     #os.path.join(BASE_DIR, 'static'),
     '/opt/project/pypyjs-release',
+    '/opt/project/front-end',
 ]
 
 STATICFILES_FINDERS = [
