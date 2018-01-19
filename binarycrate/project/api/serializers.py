0ecf8dab4a3dfa29f7223481c95e8b7892a53df6
Mark Lockett <mlockett@bigpond.com>
2018-01-19 14:35:19 +0800

    Move the serialisers file to a more appropriate location

--- binarycrate/project/api/serializers.py
+++ binarycrate/project/api/serializers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals, print_function
 
-from .models import Project
+from ..models import Project
 from rest_framework import serializers
 
 
--- binarycrate/project/api/views.py
+++ binarycrate/project/api/views.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, unicode_literals, print_function
 
 from ..models import Project
-from ..serializers import ProjectSerializer
+from .serializers import ProjectSerializer
 from rest_framework import mixins
 from rest_framework import generics
 
