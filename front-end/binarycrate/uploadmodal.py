a2804bb9c135a03901546b0592f3c01ac5b6f8ae
Mark Lockett <mlockett@bigpond.com>
2018-05-05 17:15:58 +0800

    Get viewing the uploaded files to work

--- front-end/binarycrate/uploadmodal.py
+++ front-end/binarycrate/uploadmodal.py
@@ -1,26 +1,60 @@
 from __future__ import unicode_literals, absolute_import, print_function
 from cavorite.HTML import *
-from cavorite import c, t, Router
+from cavorite import c, t, Router, timeouts
 try:
     import js
 except ImportError:
     js = None
-from cavorite.ajaxget import ajaxpost
+from cavorite.ajaxget import ajaxpost, ajaxget
+import json
+
+
+class UploadedImage(div):
+    def __init__(self, image):
+        self.image = image
+        super(UploadedImage, self).__init__({'style': {'width':'150px', 
+                                                       'height': '150px',
+                                                       #'border': '1px solid red'
+                                                       'overflow': 'hidden',
+                                                       }})
+
+    def get_children(self):
+        return [
+                 img({'src': self.image['image_url'],
+                      'style': {'width': '130px',
+                                'height': '105px',
+                                'margin': '10px',
+                                }
+                      }), 
+                 p({'style': {'font-size': '12px'}}, self.image['name'])
+               ]
 
 
 class UploadModal(object):
     def __init__(self, ownerview):
         self.ownerview = ownerview
+        timeouts.set_timeout(lambda : self.query_images(), 1)
+        self.images = []
+
+    def image_list_callback_handler(self, xmlhttp, response):
+        if xmlhttp.status >= 200 and xmlhttp.status <= 299:
+            self.images = json.loads(str(xmlhttp.responseText))
+            self.ownerview.mount_redraw()
+            Router.router.ResetHashChange()
+
+    def get_images_for_display(self):
+        return [UploadedImage(image) for image in self.images]      
+
+    def query_images(self):
+        ajaxget('/api/projects/image-list/' + self.ownerview.get_project()['id'],
+                self.image_list_callback_handler)
 
     def on_submit_upload(self, e):
         def dummy_result_handler(xmlhttp, response):
             print('dummy_result_handler called')
 
-        print('on_submit_upload called')
         e.preventDefault()
         file_upload_element = js.globals.document.getElementById('image-upload-form-control')
-        print('on_submit_upload files=',file_upload_element.files)
-        print('on_submit_upload files[0]=',file_upload_element.files[0])
         f = file_upload_element.files[0]
         form_data = {'name': f.name, 'project': self.ownerview.get_project()['id'], 'file_data': f}
         ajaxpost('/api/projects/image/', form_data, dummy_result_handler)
@@ -33,8 +67,7 @@ class UploadModal(object):
                       div({'onclick': self.ownerview.close_upload_modal, 'class': 'upload-files-modal-container'}, [
                         div({'class': 'upload-files-modal-content'}, [
                           div({'style': {'width': '100%', 'height': 'calc(100% - 40px - 10px)',
-                                         'padding': '20px', 'box-shadow': 'inset 0 0 10px darkgrey'}}, [
-                          ]),
+                                         'padding': '20px', 'box-shadow': 'inset 0 0 10px darkgrey'}}, self.get_images_for_display),
                           div({'style': {'width': '100%', 'height': '40px', 'padding-left': '20px',
                                          'padding-right': '20px', 'margin-top': '10px'}}, [
                             div({'style': {'width': 'calc(100% - 120px)', 'height': '40px',
