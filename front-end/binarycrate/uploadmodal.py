db7749fd3ace53890014beb725aad06ac2b5c100
Mark Lockett <mark@deliveryengine.net>
2018-07-28 09:47:18 +0800

    Move the ContextMenu to be a more generic class #M21-90 #M21-105

--- front-end/binarycrate/uploadmodal.py
+++ front-end/binarycrate/uploadmodal.py
@@ -10,41 +10,9 @@ import json
 import traceback
 import sys
 import uuid
+from binarycrate.controls import ContextMenu
 
 #TODO: There appear to be no unit tests for anything in this file. Please make them
-
-
-class ContextMenu2(div):
-    # An improved context menu which can hopefully take care of collapsing itself
-    def __init__(self, owner, posx, posy, menu_items, *args, **kwargs):
-        self.owner = owner
-        self.menu_items = menu_items
-        self.posx = posx
-        self.posy = posy
-        super(ContextMenu2, self).__init__({'onclick': self.owner.close_context_menu,
-                                           'style': {'position': 'fixed',
-                                                     'left': '0',
-                                                     'top': '0',
-                                                     'height': '100%',
-                                                     'width': '100%',
-                                                     'background-color': 'rgba(0, 0, 0, 0)',
-                                                     'padding': '40px',
-                                                     'z-index':'10001'}}, *args, **kwargs)
-
-    def get_children(self):
-        menu_items = [
-                                li({'class': "context-menu__item"}, [
-                                  a({'href': get_current_hash(), 'class': "context-menu__link", 'onclick': mi[1]}, [
-                                    #i({'class': 'fa fa-eye'}),
-                                    t(mi[0]),
-                                  ]),
-                                ]) for mi in self.menu_items]
-
-        return [
-                 nav({'class': "context-menu", 'style': 'left: {}px; top:{}px'.format(self.posx, self.posy)}, [
-                   ul({'class': 'context-menu__items'}, menu_items),
-                 ])
-               ]
 
 
 class ConfirmRenamePopup(div):
@@ -164,18 +132,6 @@ class UploadedImage(div):
                                                        'display': 'inline-block',
                                                        }})
 
-    def xy_from_e(self, e):
-        #TODO: Shared code put in a library
-        if e.pageX or e.pageY:
-            posx = e.pageX
-            posy = e.pageY
-        elif e.clientX or e.clientY:
-            posx = e.clientX + js.globals.document.body.scrollLeft + \
-                               js.globals.document.documentElement.scrollLeft
-            posy = e.clientY + js.globals.document.body.scrollTop + \
-                               js.globals.document.documentElement.scrollTop
-        return posx, posy
-
     def rename_image(self, e):
         self.owner.popup = ConfirmRenamePopup(self.owner, self.image)
         """
@@ -197,8 +153,8 @@ class UploadedImage(div):
         self.owner.popup = ConfirmDeletePopup(self.owner, self.image)
 
     def popup_contextmenu(self, e):
-        posx, posy = self.xy_from_e(e)
-        self.owner.ownerview.context_menu = ContextMenu2(self, posx, posy, (
+        posx, posy = ContextMenu.xy_from_e(e)
+        self.owner.ownerview.context_menu = ContextMenu(self, posx, posy, (
                                         ('Rename Image', self.rename_image),
                                         ('Delete Image', self.delete_image),
                                         ))
