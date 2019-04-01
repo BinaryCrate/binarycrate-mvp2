6a1288ec25b68f88dba03fdb8ac8fe21947ad3dc
Mark Lockett <mark@deliveryengine.net>
2019-04-01 15:16:06 +0800

    Add test for class with more than one function

--- binarycrate/parser/tests/test_apis.py
+++ binarycrate/parser/tests/test_apis.py
@@ -38,3 +38,20 @@ class ParserTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0], ["__init__", 2])
+
+
+    def test_parse_class_with_two_functions(self):
+        url = reverse('api:parser-get-member-functions')
+        data = {'content': """class MyForm(Form):
+    def __init__(self, *args, **kwargs):
+        super(MyForm, self).__init__(*args, **kwargs)
+        self.name = ""
+
+    def button1_onclick(self, e):
+        pass
+""" }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0], ["__init__", 2])
+        self.assertEqual(response.data[1], ["button1_onclick", 6])
